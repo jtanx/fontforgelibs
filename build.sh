7126cfecc2a1cb739aa32df63962f81cab178703
Jeremy Tan <jtanx@outlook.com>
2019-04-13 18:15:06 +1000

    Update libspiro and libuninameslist

--- build.sh
+++ build.sh
@@ -33,23 +33,23 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PACKAGES=(
     mingw-w64-libspiro-git
     mingw-w64-libuninameslist-git
-    mingw-w64-xorg-util-macros-git
-    mingw-w64-x11proto-git
-    mingw-w64-renderproto-git
-    mingw-w64-kbproto-git
-    mingw-w64-inputproto-git
-    mingw-w64-xextproto-git
-    mingw-w64-xf86bigfontproto-git
-    mingw-w64-xcb-proto-git
-    mingw-w64-libxau-git
-    mingw-w64-xtrans-git
-    mingw-w64-libxcb-git
-    mingw-w64-libx11-git
-    mingw-w64-libxext-git
-    mingw-w64-libxrender-git
-    mingw-w64-libxft-git
-    mingw-w64-cairo-x11
-    mingw-w64-pango-x11
+    #mingw-w64-xorg-util-macros-git
+    #mingw-w64-x11proto-git
+    #mingw-w64-renderproto-git
+    #mingw-w64-kbproto-git
+    #mingw-w64-inputproto-git
+    #mingw-w64-xextproto-git
+    #mingw-w64-xf86bigfontproto-git
+    #mingw-w64-xcb-proto-git
+    #mingw-w64-libxau-git
+    #mingw-w64-xtrans-git
+    #mingw-w64-libxcb-git
+    #mingw-w64-libx11-git
+    #mingw-w64-libxext-git
+    #mingw-w64-libxrender-git
+    #mingw-w64-libxft-git
+    #mingw-w64-cairo-x11
+    #mingw-w64-pango-x11
 )
 
 # Colourful text
